3b588aa5d584b60e6e0411ef8d1d03cecc787736
“zsp” <181459193@qq.com>
2024-03-15 10:12:42 +0800

    调整路径，修改uvr5重采样过程中出现非有限值的问题

--- tools/uvr5/lib/lib_v5/spec_utils.py
+++ tools/uvr5/lib/lib_v5/spec_utils.py
@@ -397,14 +397,19 @@ def cmb_spectrogram_to_wave(spec_m, mp, extra_bins_h=None, extra_bins=None):
             sr = mp.param["band"][d + 1]["sr"]
             if d == 1:  # lower
                 spec_s = fft_lp_filter(spec_s, bp["lpf_start"], bp["lpf_stop"])
-                wave = librosa.resample(
-                    spectrogram_to_wave(
+                A = spectrogram_to_wave(
                         spec_s,
                         bp["hl"],
                         mp.param["mid_side"],
                         mp.param["mid_side_b2"],
                         mp.param["reverse"],
-                    ),
+                    )
+                if not np.all(np.isfinite(A)):
+                    # 将非有限数值替换为零或其他有限数值
+                    A = np.nan_to_num(A)
+
+                wave = librosa.resample(
+                    A,
                     orig_sr   = bp["sr"],
                     target_sr = sr,
                     res_type  = "sinc_fastest",
